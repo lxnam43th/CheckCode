11efe1f6fbab4b0f408a9d492d41e29d8ca735b2
Le Xuan Nam <NamLX@Hoya.com.vn>
2018-02-05 08:57:46 +0700

    test2

--- GenerateQCDailyInformationConsole/Class1.vb
+++ GenerateQCDailyInformationConsole/Class1.vb
@@ -1,6 +1,8 @@
 ﻿Public Class Class1
     Sub send_data()
-        Dim i As Integer
+        Dim i, m As Integer
         i = 1
+        i = 50 + 3
+        m = 0
     End Sub
 End Class
---
+++ GenerateQCDailyInformationConsole/Class2.vb
@@ -0,0 +1,7 @@
+﻿Public Class Class2
+    Sub send_email()
+        Dim n, m As Integer
+        n = 0
+        m = 0
+    End Sub
+End Class
